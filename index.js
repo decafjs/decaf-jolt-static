f4575936ca6fe4462a52eef469e31fa1fad9ed7a
Michael Schwartz <mykes@sportstwo.com>
2014-10-05 07:24:15 -0700

    fix bower.json

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-module.exports = require('lib/StaticServer.js');
+/*global decaf, require, exports:true */
 
+decaf.extend(exports, {
+    StaticServer: require('lib/StaticServer').StaticServer,
+    StaticFile  : require('lib/StaticServer').StaticFile
+});
